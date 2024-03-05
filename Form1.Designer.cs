46167ef3ceeddb2e1b3f0651154f9abdb3e068d0
USUARIO <USUARIO@ARIPC>
2024-03-05 19:11:44 -0400

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace APP_EJER_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Mostrar = new System.Windows.Forms.Button();
+            this.btn_Limpiar = new System.Windows.Forms.Button();
+            this.btn_Salir = new System.Windows.Forms.Button();
+            this.txt_Matricula = new System.Windows.Forms.TextBox();
+            this.txt_Nombre = new System.Windows.Forms.TextBox();
+            this.txt_Apellido = new System.Windows.Forms.TextBox();
+            this.txt_Carrera = new System.Windows.Forms.TextBox();
+            this.lbl_Matricula = new System.Windows.Forms.Label();
+            this.lbl_Nombre = new System.Windows.Forms.Label();
+            this.lbl_Apellido = new System.Windows.Forms.Label();
+            this.lbl_Carrera = new System.Windows.Forms.Label();
+            this.lbl_Datos = new System.Windows.Forms.Label();
+            this.lbl_Edad = new System.Windows.Forms.Label();
+            this.txt_Edad = new System.Windows.Forms.TextBox();
+            this.txt_Datos = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_Mostrar
+            // 
+            this.btn_Mostrar.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btn_Mostrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Mostrar.Font = new System.Drawing.Font("Mongolian Baiti", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Mostrar.Location = new System.Drawing.Point(107, 336);
+            this.btn_Mostrar.Name = "btn_Mostrar";
+            this.btn_Mostrar.Size = new System.Drawing.Size(88, 34);
+            this.btn_Mostrar.TabIndex = 0;
+            this.btn_Mostrar.Text = "Mostrar";
+            this.btn_Mostrar.UseVisualStyleBackColor = false;
+            this.btn_Mostrar.Click += new System.EventHandler(this.btn_Mostrar_Click);
+            // 
+            // btn_Limpiar
+            // 
+            this.btn_Limpiar.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btn_Limpiar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Limpiar.Font = new System.Drawing.Font("Mongolian Baiti", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Limpiar.Location = new System.Drawing.Point(298, 336);
+            this.btn_Limpiar.Name = "btn_Limpiar";
+            this.btn_Limpiar.Size = new System.Drawing.Size(88, 34);
+            this.btn_Limpiar.TabIndex = 1;
+            this.btn_Limpiar.Text = "Limpiar";
+            this.btn_Limpiar.UseVisualStyleBackColor = false;
+            this.btn_Limpiar.Click += new System.EventHandler(this.btn_Limpiar_Click);
+            // 
+            // btn_Salir
+            // 
+            this.btn_Salir.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btn_Salir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Salir.Font = new System.Drawing.Font("Mongolian Baiti", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Salir.Location = new System.Drawing.Point(485, 336);
+            this.btn_Salir.Name = "btn_Salir";
+            this.btn_Salir.Size = new System.Drawing.Size(88, 34);
+            this.btn_Salir.TabIndex = 2;
+            this.btn_Salir.Text = "Salir";
+            this.btn_Salir.UseVisualStyleBackColor = false;
+            this.btn_Salir.Click += new System.EventHandler(this.btn_Salir_Click);
+            // 
+            // txt_Matricula
+            // 
+            this.txt_Matricula.Font = new System.Drawing.Font("Mongolian Baiti", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Matricula.Location = new System.Drawing.Point(144, 89);
+            this.txt_Matricula.Name = "txt_Matricula";
+            this.txt_Matricula.Size = new System.Drawing.Size(129, 22);
+            this.txt_Matricula.TabIndex = 3;
+            // 
+            // txt_Nombre
+            // 
+            this.txt_Nombre.Font = new System.Drawing.Font("Mongolian Baiti", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Nombre.Location = new System.Drawing.Point(144, 172);
+            this.txt_Nombre.Name = "txt_Nombre";
+            this.txt_Nombre.Size = new System.Drawing.Size(129, 22);
+            this.txt_Nombre.TabIndex = 4;
+            // 
+            // txt_Apellido
+            // 
+            this.txt_Apellido.Font = new System.Drawing.Font("Mongolian Baiti", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Apellido.Location = new System.Drawing.Point(144, 215);
+            this.txt_Apellido.Name = "txt_Apellido";
+            this.txt_Apellido.Size = new System.Drawing.Size(129, 22);
+            this.txt_Apellido.TabIndex = 5;
+            // 
+            // txt_Carrera
+            // 
+            this.txt_Carrera.Font = new System.Drawing.Font("Mongolian Baiti", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Carrera.Location = new System.Drawing.Point(144, 256);
+            this.txt_Carrera.Name = "txt_Carrera";
+            this.txt_Carrera.Size = new System.Drawing.Size(129, 22);
+            this.txt_Carrera.TabIndex = 6;
+            // 
+            // lbl_Matricula
+            // 
+            this.lbl_Matricula.AutoSize = true;
+            this.lbl_Matricula.Font = new System.Drawing.Font("Mongolian Baiti", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Matricula.Location = new System.Drawing.Point(68, 92);
+            this.lbl_Matricula.Name = "lbl_Matricula";
+            this.lbl_Matricula.Size = new System.Drawing.Size(69, 14);
+            this.lbl_Matricula.TabIndex = 7;
+            this.lbl_Matricula.Text = "Matricula";
+            // 
+            // lbl_Nombre
+            // 
+            this.lbl_Nombre.AutoSize = true;
+            this.lbl_Nombre.Font = new System.Drawing.Font("Mongolian Baiti", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Nombre.Location = new System.Drawing.Point(68, 175);
+            this.lbl_Nombre.Name = "lbl_Nombre";
+            this.lbl_Nombre.Size = new System.Drawing.Size(56, 14);
+            this.lbl_Nombre.TabIndex = 8;
+            this.lbl_Nombre.Text = "Nombre";
+            // 
+            // lbl_Apellido
+            // 
+            this.lbl_Apellido.AutoSize = true;
+            this.lbl_Apellido.Font = new System.Drawing.Font("Mongolian Baiti", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Apellido.Location = new System.Drawing.Point(68, 218);
+            this.lbl_Apellido.Name = "lbl_Apellido";
+            this.lbl_Apellido.Size = new System.Drawing.Size(63, 14);
+            this.lbl_Apellido.TabIndex = 9;
+            this.lbl_Apellido.Text = "Apellido";
+            // 
+            // lbl_Carrera
+            // 
+            this.lbl_Carrera.AutoSize = true;
+            this.lbl_Carrera.Font = new System.Drawing.Font("Mongolian Baiti", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Carrera.Location = new System.Drawing.Point(68, 259);
+            this.lbl_Carrera.Name = "lbl_Carrera";
+            this.lbl_Carrera.Size = new System.Drawing.Size(53, 14);
+            this.lbl_Carrera.TabIndex = 10;
+            this.lbl_Carrera.Text = "Carrera";
+            // 
+            // lbl_Datos
+            // 
+            this.lbl_Datos.AutoSize = true;
+            this.lbl_Datos.Font = new System.Drawing.Font("Mongolian Baiti", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Datos.Location = new System.Drawing.Point(409, 132);
+            this.lbl_Datos.Name = "lbl_Datos";
+            this.lbl_Datos.Size = new System.Drawing.Size(48, 16);
+            this.lbl_Datos.TabIndex = 11;
+            this.lbl_Datos.Text = "Datos";
+            // 
+            // lbl_Edad
+            // 
+            this.lbl_Edad.AutoSize = true;
+            this.lbl_Edad.Font = new System.Drawing.Font("Mongolian Baiti", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Edad.Location = new System.Drawing.Point(68, 130);
+            this.lbl_Edad.Name = "lbl_Edad";
+            this.lbl_Edad.Size = new System.Drawing.Size(39, 14);
+            this.lbl_Edad.TabIndex = 13;
+            this.lbl_Edad.Text = "Edad";
+            // 
+            // txt_Edad
+            // 
+            this.txt_Edad.Font = new System.Drawing.Font("Mongolian Baiti", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Edad.Location = new System.Drawing.Point(144, 130);
+            this.txt_Edad.Name = "txt_Edad";
+            this.txt_Edad.Size = new System.Drawing.Size(129, 22);
+            this.txt_Edad.TabIndex = 14;
+            // 
+            // txt_Datos
+            // 
+            this.txt_Datos.Font = new System.Drawing.Font("Mongolian Baiti", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Datos.Location = new System.Drawing.Point(368, 159);
+            this.txt_Datos.Multiline = true;
+            this.txt_Datos.Name = "txt_Datos";
+            this.txt_Datos.Size = new System.Drawing.Size(129, 78);
+            this.txt_Datos.TabIndex = 15;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Mongolian Baiti", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(262, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(186, 20);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Datos del Estudiante";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Azure;
+            this.ClientSize = new System.Drawing.Size(708, 397);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_Datos);
+            this.Controls.Add(this.txt_Edad);
+            this.Controls.Add(this.lbl_Edad);
+            this.Controls.Add(this.lbl_Datos);
+            this.Controls.Add(this.lbl_Carrera);
+            this.Controls.Add(this.lbl_Apellido);
+            this.Controls.Add(this.lbl_Nombre);
+            this.Controls.Add(this.lbl_Matricula);
+            this.Controls.Add(this.txt_Carrera);
+            this.Controls.Add(this.txt_Apellido);
+            this.Controls.Add(this.txt_Nombre);
+            this.Controls.Add(this.txt_Matricula);
+            this.Controls.Add(this.btn_Salir);
+            this.Controls.Add(this.btn_Limpiar);
+            this.Controls.Add(this.btn_Mostrar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Mostrar;
+        private System.Windows.Forms.Button btn_Limpiar;
+        private System.Windows.Forms.Button btn_Salir;
+        private System.Windows.Forms.TextBox txt_Matricula;
+        private System.Windows.Forms.TextBox txt_Nombre;
+        private System.Windows.Forms.TextBox txt_Apellido;
+        private System.Windows.Forms.TextBox txt_Carrera;
+        private System.Windows.Forms.Label lbl_Matricula;
+        private System.Windows.Forms.Label lbl_Nombre;
+        private System.Windows.Forms.Label lbl_Apellido;
+        private System.Windows.Forms.Label lbl_Carrera;
+        private System.Windows.Forms.Label lbl_Datos;
+        private System.Windows.Forms.Label lbl_Edad;
+        private System.Windows.Forms.TextBox txt_Edad;
+        private System.Windows.Forms.TextBox txt_Datos;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace APP_EJER_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Mostrar_Click(object sender, EventArgs e)
+        {
+            // Capturar los datos del estudiante desde los TextBox
+            string matricula = txt_Matricula.Text;
+            string nombre = txt_Nombre.Text;
+            string apellidos = txt_Apellido.Text;
+            int edad = Convert.ToInt32(txt_Edad.Text); // Convertir a entero          
+            string carrera = txt_Carrera.Text;
+
+            // Construir el mensaje a mostrar en el Label
+            string mensaje = $"Matrícula: {matricula}\n" +
+                             $"Nombre: {nombre}\n" +
+                             $"Apellido: {apellidos}\n" +
+                             $"Edad: {edad}\n" +
+                             $"Carrera: {carrera}";
+
+            // Mostrar el mensaje en el Label
+            txt_Datos.Text = mensaje;
+        }
+
+        private void btn_Limpiar_Click(object sender, EventArgs e)
+        {
+            txt_Matricula.Clear();
+            txt_Nombre.Clear();
+            txt_Apellido.Clear();
+            txt_Edad.Clear();
+            txt_Carrera.Clear();
+            txt_Datos.Clear();
+        }
+
+        private void btn_Salir_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+        
+    }
+}
